1fd3a6e2c723334393aabe72bdaa033da5f233f2
tczyz <tomczyz97@gmail.com>
2022-11-28 01:42:53 +0100

    add prototype of object grasping

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::f32::consts::PI;
-
+use bevy::prelude::shape::Icosphere;
 use bevy::prelude::*;
+use bevy::render::mesh::shape::{Box};
 
-use leap_input::leap_controller_plugin::{HandsOrigin, LeapControllerPlugin};
+use leap_input::leap_controller_plugin::{HandsData, HandsOrigin, LeapControllerPlugin};
 
 mod helpers;
+mod shape;
 
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
-pub const CAMERA_ORIGIN: Transform = Transform::from_xyz(0., 500., 500.0);
+pub const TABLE_SIZE: [f32; 3] = [1600., 700., 1600.];
+pub const HANDS_DISTANCE: f32 = 800.;
+pub const CAMERA_ORIGIN: Transform = Transform::from_xyz(0., 350., 500.);
 
 fn main() {
     App::new()
@@ -27,6 +30,7 @@ fn main() {
         // .add_system(move_camera)
         // .add_system(orbit_camera)
         .add_system(adjust_hands_origin_to_camera_transform)
+        .add_system(analyze_digits_position)
         .run();
 }
 
@@ -34,40 +38,104 @@ fn main() {
 #[derive(Component)]
 pub struct PlayerCamera;
 
+#[derive(Component)]
+pub struct ObjectBounds;
+
+fn analyze_digits_position(
+    hands_res: Res<HandsData>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    meshes: Res<Assets<Mesh>>,
+    bounds_query: Query<(&Handle<Mesh>, &Handle<StandardMaterial>), With<ObjectBounds>>,
+) {
+    if hands_res.hands.len() == 0 {
+        return;
+    }
+
+    let right_finger_tips = hands_res.hands[0]
+        .digits
+        .iter()
+        .map(|digit| digit.distal.next_joint)
+        .collect::<Vec<Vec3>>();
+
+    // if there are at least 3 finger tips within bounds, then change transparency
+    let finger_tips_inside_bounds = right_finger_tips
+        .iter()
+        .filter(|vec3| vec3.distance(Vec3::new(100., 250., 0.)) < 35.)
+        .count();
+
+    let (_mesh_handle, material_handle) = bounds_query.single();
+
+    // todo: get this only if value has changed?..
+    let some_color = &mut materials.get_mut(material_handle).unwrap().base_color;
+
+    if finger_tips_inside_bounds >= 3 {
+        some_color.set_a(0.8);
+    } else {
+        some_color.set_a(0.4);
+    }
+
+    // for (_mesh_handle, material_handle) in bounds_query.iter() {
+    //     // let x = meshes.get(mesh_handle).unwrap();
+    //
+    //     let some_color = &mut materials.get_mut(material_handle).unwrap().base_color;
+    //     some_color.set_a(0.8);
+    // }
+}
+
 fn spawn_basic_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // main table
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 150.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 50.0 })),
-        material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
-        transform: Transform::from_xyz(0.0, 50., 0.0),
+        mesh: meshes.add(Mesh::from(Box::new(TABLE_SIZE[0], TABLE_SIZE[1], TABLE_SIZE[2]))),
+        transform: Transform::from_xyz(0., -TABLE_SIZE[1], 0.),
+        material: materials.add(Color::rgb_u8(50, 224, 229).into()),
         ..default()
     });
 
     commands.spawn(PointLightBundle {
         point_light: PointLight {
-            intensity: 9000.0,
-            range: 1000.,
+            intensity: 2500.0,
+            range: 2000.,
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(80.0, 160.0, 80.0),
+        transform: Transform::from_xyz(0., TABLE_SIZE[1] + 200., 0.),
         ..default()
     });
+
+    // main gizmo
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(Icosphere {
+                radius: 20.,
+                subdivisions: 12,
+            })),
+            transform: Transform::from_xyz(100., 250., 0.),
+            material: materials.add(Color::rgb_u8(50, 224, 229).into()),
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn((
+                PbrBundle {
+                    mesh: meshes.add(Mesh::from(Icosphere {
+                        radius: 35.,
+                        subdivisions: 12,
+                    })),
+                    material: materials.add(Color::rgba_u8(172, 229, 88, 50).into()),
+                    ..default()
+                },
+                ObjectBounds,
+            ));
+        });
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Camera3dBundle {
-            transform: CAMERA_ORIGIN.looking_at(Vec3::ZERO, Vec3::Y),
+            transform: CAMERA_ORIGIN.looking_at(Vec3::Y * 200., Vec3::Y),
             ..default()
         },
         PlayerCamera,
@@ -89,9 +157,12 @@ fn adjust_hands_origin_to_camera_transform(
     let mut hands_origin_transform = hand_origin_query.single_mut();
     let camera_transform = camera_query.single();
 
-    *hands_origin_transform = Transform {
-        translation: camera_transform.translation + camera_transform.forward() * 500.,
-        rotation: camera_transform.rotation * Quat::from_euler(EulerRot::YXZ, 0., PI / 2., 0.),
-        ..default()
-    };
+    // *hands_origin_transform = Transform {
+    //     translation: camera_transform.translation + camera_transform.forward() * HANDS_DISTANCE,// - Vec3::Y * 300.,
+    //     rotation: Quat::from_axis_angle(Vec3::X, PI),
+    //     // rotation: camera_transform.rotation * Quat::from_euler(EulerRot::YXZ, 0., PI / 4., 0.),
+    //     ..default()
+    // };
+    let transform = Transform::IDENTITY;
+    *hands_origin_transform = transform;
 }
